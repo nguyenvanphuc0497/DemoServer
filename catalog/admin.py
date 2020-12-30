fe9009e25cac2a2f35ce83cfc74561c57600d515
Nguyen Van Phuc <phuc.nguyen3@asiantech.vn>
2020-12-30 10:02:04 +0700

    Custom admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Book, BookInstance, Author
+from .models import Book, BookInstance, Author, Genre
 
 # Register your models here.
-admin.site.register(Book)
 admin.site.register(BookInstance)
-admin.site.register(Author)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
